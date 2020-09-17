5e271c58deaf984a30adac4c776c53dd0741e02c
hodory <mo02@outlook.kr>
2020-09-18 03:07:33 +0900

    edit : logging before init

--- database/index.js
+++ database/index.js
@@ -7,10 +7,10 @@ if (!firebase.apps.length) {
     clientEmail: process.env.FIREBASE_API_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_API_PRIVATE_KEY,
   };
+  console.log(serviceAccountObject);
   firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccountObject),
     databaseURL: FIREBASE_API_URL,
   });
-  console.log(serviceAccountObject);
 }
 export const database = firebase.database();
