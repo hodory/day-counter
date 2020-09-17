db59b9cc030a4c069613b65d364a745a60d3e3c3
hodory <mo02@outlook.kr>
2020-09-18 02:54:24 +0900

    edit : logging when firebase initalize error

--- database/index.js
+++ database/index.js
@@ -2,13 +2,19 @@ const firebase = require("firebase-admin");
 const { FIREBASE_API_URL } = require("../const");
 
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    credential: firebase.credential.cert({
-      projectId: process.env.FIREBASE_API_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_API_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_API_PRIVATE_KEY,
-    }),
-    databaseURL: FIREBASE_API_URL,
-  });
+  const serviceAccountObject = {
+    projectId: process.env.FIREBASE_API_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_API_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_API_PRIVATE_KEY,
+  };
+  try {
+    firebase.initializeApp({
+      credential: firebase.credential.cert(serviceAccountObject),
+      databaseURL: FIREBASE_API_URL,
+    });
+  } catch (e) {
+    console.log(e);
+    console.error(serviceAccountObject);
+  }
 }
 export const database = firebase.database();
