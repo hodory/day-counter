7d2b4f006c3171cf9fa60d774478180c4053892c
hodory <mo02@outlook.kr>
2020-09-18 03:03:57 +0900

    edit : without try-catch

--- database/index.js
+++ database/index.js
@@ -7,14 +7,9 @@ if (!firebase.apps.length) {
     clientEmail: process.env.FIREBASE_API_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_API_PRIVATE_KEY,
   };
-  try {
-    firebase.initializeApp({
-      credential: firebase.credential.cert(serviceAccountObject),
-      databaseURL: FIREBASE_API_URL,
-    });
-  } catch (e) {
-    console.log(e);
-    console.error(serviceAccountObject);
-  }
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccountObject),
+    databaseURL: FIREBASE_API_URL,
+  });
 }
 export const database = firebase.database();
